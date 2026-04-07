480339a467b913cc855a9adbf231f27931263dde
Владислава <olav80@internet.ru>
2026-04-08 00:06:26 +0300

    created 'LoginTests.py'

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -15,6 +15,37 @@ class LoginPageLocators:
     REGISTER_VK = (By.XPATH, '//a[@data-module="registration/vkconnect"]')
     REGISTER_MAILRU = (By.XPATH, '//a[@data-l="t,mailru"]')
     REGISTER_YANDEX = (By.XPATH, '//a[@data-provider="YANDEX"]')
+    ERROR_LOGIN = (By.XPATH, '//div/span[text()="Введите логин"]')
 
 
 class LoginPageHelper(BasePage):
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.ENTER_BUTTON)
+        self.find_element(LoginPageLocators.ENTER_QRCODE_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.EYE_HIDE_PASSWORD)
+        self.find_element(LoginPageLocators.LINK_ENTER)
+        self.find_element(LoginPageLocators.LINK_ENTER_QRCODE)
+        self.find_element(LoginPageLocators.LINK_CANNOT_ENTER)
+        self.find_element(LoginPageLocators.REGISTER)
+        self.find_element(LoginPageLocators.REGISTER_VK)
+        self.find_element(LoginPageLocators.REGISTER_MAILRU)
+        self.find_element(LoginPageLocators.REGISTER_YANDEX)
+
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.ENTER_BUTTON).click()
+
+
+    def fill_login_field(self):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys("Romashka")
+
+
+    def get_error_text(self):
+        return self.find_element(LoginPageLocators.ERROR_LOGIN).text
---
+++ tests/LoginTests.py
@@ -0,0 +1,21 @@
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+
+BASE_URL = "https://ok.ru/"
+EMPTY_LOGIN_ERROR = "Введите логин"
+EMPTY_PASSWORD_ERROR = "Введите пароль"
+
+def test_empty_login_and_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.click_login()
+    assert login_page.get_error_text() == EMPTY_LOGIN_ERROR
+
+
+def test_empty_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.fill_login_field()
+    login_page.click_login()
+    assert login_page.get_error_text() == EMPTY_PASSWORD_ERROR
