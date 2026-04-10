39e123d9b3afc16b909d00ffcad4bb5a1df0c76f
Владислава <olav80@internet.ru>
2026-04-10 19:09:01 +0300

    added tests with asserts

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,6 +1,6 @@
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
-
+import allure
 
 class LoginPageLocators:
     ENTER_BUTTON = (By.XPATH, '//button[@label="Войти"]')
@@ -12,11 +12,13 @@ class LoginPageLocators:
     LINK_ENTER_QRCODE = (By.XPATH, '//a[@title="QR-код"]')
     LINK_CANNOT_ENTER = (By.XPATH, '//button[@aria-label="Не получается войти?"]')
     REGISTER = (By.XPATH, '//button/span/span[text()="Зарегистрироваться"]')
-    REGISTER_VK = (By.XPATH, '//a[@data-module="registration/vkconnect"]')
-    REGISTER_MAILRU = (By.XPATH, '//a[@data-l="t,mailru"]')
-    REGISTER_YANDEX = (By.XPATH, '//a[@data-provider="YANDEX"]')
+    VK = (By.XPATH, '//a[@data-module="registration/vkconnect"]')
+    MAILRU = (By.XPATH, '//a[@data-l="t,mailru"]')
+    YANDEX = (By.XPATH, '//a[@data-provider="YANDEX"]')
     ERROR_LOGIN = (By.XPATH, '//div/span[text()="Введите логин"]')
     ERROR_PASSWORD = (By.XPATH, '//div/span[text()="Введите пароль"]')
+    RECOVERY = (By.XPATH, "//div[text()='Восстановление доступа']")
+    QR_CODE_IMAGE = (By.XPATH, "//div/img[@class ='qr_code_image']")
 
 
 class LoginPageHelper(BasePage):
@@ -35,21 +37,65 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.LINK_ENTER_QRCODE)
         self.find_element(LoginPageLocators.LINK_CANNOT_ENTER)
         self.find_element(LoginPageLocators.REGISTER)
-        self.find_element(LoginPageLocators.REGISTER_VK)
-        self.find_element(LoginPageLocators.REGISTER_MAILRU)
-        self.find_element(LoginPageLocators.REGISTER_YANDEX)
+        self.find_element(LoginPageLocators.VK)
+        self.find_element(LoginPageLocators.MAILRU)
+        self.find_element(LoginPageLocators.YANDEX)
 
 
+    @allure.step('Нажимаем на кнопку"Войти"')
     def click_login(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.ENTER_BUTTON).click()
 
+    @allure.step('Переходим по ссылке "QR-код"')
+    def link_enter_qrcode(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.LINK_ENTER_QRCODE).click()
 
+    @allure.step('Заполняем поле логина')
     def fill_login_field(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys("Romashka")
 
-
+    @allure.step('Получаем текст ошибки')
     def get_error_text_login(self):
+        self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_LOGIN).text
 
+    @allure.step('Получаем текст ошибки')
     def get_error_text_password(self):
+        self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_PASSWORD).text
+
+    @allure.step('Нажимаем на кнопку "Войти по QR-коду"')
+    def enter_button_qrcode(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.ENTER_QRCODE_BUTTON).click()
+
+    @allure.step('Нажимаем на кнопку "Не получается войти?"')
+    def link_cannot_enter(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.LINK_CANNOT_ENTER).click()
+
+    @allure.step('Нажимаем на кнопку "Зарегистрироваться"')
+    def register(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.REGISTER).click()
+
+    @allure.step('Нажимаем на кнопку "Войти через VK ID"')
+    def vk_id(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.VK).click()
+
+    @allure.step('Нажимаем на кнопку "Войти через почту Mail.ru"')
+    def mail_ru(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.MAILRU).click()
+
+    @allure.step('Нажимаем на кнопку "Войти через Яндекс"')
+    def yandex(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.YANDEX).click()
+
+
+
--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,21 +1,86 @@
 from core.BaseTest import browser
 from pages.BasePage import BasePage
-from pages.LoginPage import LoginPageHelper
+from pages.LoginPage import LoginPageHelper, LoginPageLocators
+import allure
 
 BASE_URL = "https://ok.ru/"
 EMPTY_LOGIN_ERROR = "Введите логин"
 EMPTY_PASSWORD_ERROR = "Введите пароль"
 
+
+@allure.suite('Проверка формы авторизации')
+@allure.title('Проверка ошибки при пустой форме авторизации')
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     login_page = LoginPageHelper(browser)
     login_page.click_login()
     assert login_page.get_error_text_login() == EMPTY_LOGIN_ERROR
 
-
+@allure.title('Проверка ошибки при отсутствии пароля')
 def test_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     login_page = LoginPageHelper(browser)
     login_page.fill_login_field()
     login_page.click_login()
     assert login_page.get_error_text_password() == EMPTY_PASSWORD_ERROR
+
+@allure.title('Переход на страницу "QR-код" по ссылке')
+def test_go_to_qrcode_link(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.link_enter_qrcode()
+    base_page = BasePage(browser)
+    qr_code_image = base_page.find_element(LoginPageLocators.QR_CODE_IMAGE)
+    assert qr_code_image is not None
+
+@allure.title('Переход на страницу "QR-код" по кнопке')
+def test_go_to_qrcode_button(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.enter_button_qrcode()
+    base_page = BasePage(browser)
+    qr_code_image = base_page.find_element(LoginPageLocators.QR_CODE_IMAGE)
+    assert qr_code_image is not None
+
+
+@allure.title('Переход на страницу восстановления доступа')
+def test_go_to_forgot_password_page(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.link_cannot_enter()
+    base_page = BasePage(browser)
+    recovery = base_page.find_element(LoginPageLocators.RECOVERY).text
+    assert recovery == "Восстановление доступа"
+
+# переходит на vk, но assert ожидает ok.ru
+@allure.title('Переход на страницу регистрации')
+def test_go_to_register_page(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.register()
+
+
+# не успевает открыть? тормозит интеграция с vk?
+def test_enter_vk_id(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.vk_id()
+    browser.switch_to.window(browser.window_handles[-1])
+    assert "vk.com" in browser.current_url
+
+
+@allure.title('Вход через почту Mail.ru')
+def test_enter_mailru(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.mail_ru()
+    browser.switch_to.window(browser.window_handles[-1])
+    assert "mail.ru" in browser.current_url
+
+@allure.title('Вход через Яндекс')
+def test_enter_yandex(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.yandex()
+    browser.switch_to.window(browser.window_handles[-1])
+    assert "yandex" in browser.current_url
